810953502e5600bc1f349ebb4c2370d6364d2dd4
Shawn Rancatore <shawn.rancatore@gmail.com>
2013-02-23 16:47:09 -0500

    new stuff

--- reddup/scru/admin.py
+++ reddup/scru/admin.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
+#from django.contrib import admin
 from .models import *
+from django.contrib.gis import admin
 
-admin.site.register(Issue)
+admin.site.register(Issue, admin.OSMGeoAdmin)
+admin.site.register(Pledge, admin.OSMGeoAdmin)
+
 admin.site.register(IssueUser)
-admin.site.register(Pledge)
+
 admin.site.register(LocationType)
 admin.site.register(Category)
