fcf3a7c5495690dfffad270df373cdeb9a719f90
Jim Skerritt <jrskerritt@gmail.com>
2013-02-23 12:34:29 -0500

    adds more sample points to main.js

--- static/js/main.js
+++ static/js/main.js
@@ -17,6 +17,7 @@ angular.module('steelCityReddUp', ['ui'])
 function Controller($scope) {
 	$scope.mode = 'default';
 	$scope.markers = [];
+	$scope.currentMarker = null;
 
 	$scope.mapOptions = {
 		center: new google.maps.LatLng(40.4406, -79.9961),
@@ -31,9 +32,8 @@ function Controller($scope) {
 			"url",
 			function (data) {
 				$('#spinner').stop();
-				$.each(data.items, function(item){
-
-				});
+				// TODO: change to $scope.markers = data;
+				$scope.markers = samplePoints;
 			}
 		);
 	}
@@ -52,19 +52,51 @@ function Controller($scope) {
 	}
 }
 
-var samplePoint = {
-	id 				: 1,
-	description 	: "Found some graffiti on the side of an 
-					   office building on Penn Ave",
-	latLng 			: new LatLng(40.462013, 79.926331),
-	beforeImg 		: "before.jpg",
-	afterImg 		: "afterImg",
-	dateOpened		: new Date("Sat Feb 23 2013 11:48:20 GMT-0500 (EST)"),
-	dateClosed		: new Date("Sun Feb 24 2013 12:48:20 GMT-0500 (EST)"), 
-	opener			: { id: 2, username: "jim", deleted: false },
-	closer			: null,
-	cleanerId		: { id: 3, username: "nate", deleted: false },
-	category		: { id: 5, name: "Graffiti", appKey: "GRAFFITI" },
-	locationType	: { id: 5, name: "Street", appKey: "STREET" }
-	reportedTo311	: true,
-};
+var samplePoints = [
+	{
+		id 				: 1,
+		description 	: "Found some graffiti on the side of an 
+						   office building on Penn Ave",
+		latLng 			: new LatLng(40.462013, -79.926331),
+		beforeImg 		: "before.jpg",
+		afterImg 		: "after.jpg",
+		dateOpened		: new Date("Sat Feb 23 2013 11:48:20 GMT-0500 (EST)"),
+		dateClosed		: new Date("Sun Feb 24 2013 12:48:20 GMT-0500 (EST)"), 
+		opener			: { id: 2, username: "jim", deleted: false },
+		closer			: null,
+		cleanerId		: { id: 3, username: "nate", deleted: false },
+		category		: { id: 5, name: "Graffiti", appKey: "GRAFFITI" },
+		locationType	: { id: 5, name: "Street", appKey: "STREET" }
+		reportedTo311	: true,
+	},
+	{
+		id 				: 2,
+		description 	: "Large pothole on Ellsworth Ave",
+		latLng 			: new LatLng(40.457736, -79.928359‎),
+		beforeImg 		: "before.jpg",
+		afterImg 		: "after.jpg",
+		dateOpened		: new Date("Sat Feb 23 2013 13:45:44 GMT-0500 (EST)"),
+		dateClosed		: new Date("Sun Feb 24 2013 09:21:20 GMT-0500 (EST)"), 
+		opener			: { id: 3, username: "geoff", deleted: false },
+		closer			: null,
+		cleanerId		: { id: 4, username: "ben", deleted: false },
+		category		: { id: 1, name: "Pothole", appKey: "POTHOLE" },
+		locationType	: { id: 5, name: "Street", appKey: "STREET" }
+		reportedTo311	: true,
+	},
+	{
+		id 				: 3,
+		description 	: "Large pothole on Fifth Ave",
+		latLng 			: new LatLng(40.453185, -79.918220‎),
+		beforeImg 		: "before.jpg",
+		afterImg 		: "after.jpg",
+		dateOpened		: new Date("Sat Feb 23 2013 14:45:44 GMT-0500 (EST)"),
+		dateClosed		: new Date("Sun Feb 24 2013 10:21:20 GMT-0500 (EST)"), 
+		opener			: { id: 5, username: "shawn", deleted: false },
+		closer			: null,
+		cleanerId		: { id: 6, username: "david", deleted: false },
+		category		: { id: 1, name: "Pothole", appKey: "POTHOLE" },
+		locationType	: { id: 5, name: "Street", appKey: "STREET" }
+		reportedTo311	: true,
+	}
+];
