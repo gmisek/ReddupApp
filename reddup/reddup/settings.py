dde925949b31ef69026f0f7f005690cb4f86a44c
Nate Bridi <nate.bridi@gmail.com>
2013-02-23 10:36:18 -0500

    Settings

--- reddup/reddup/settings.py
+++ reddup/reddup/settings.py
@@ -101,9 +101,7 @@ ROOT_URLCONF = 'reddup.urls'
 WSGI_APPLICATION = 'reddup.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    '/Users/nate/Documents/Projects/2013 Steel City Codefest/Reddup/reddup/templates/',
 )
 
 INSTALLED_APPS = (
@@ -117,6 +115,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south'
 )
 
 # A sample logging configuration. The only tangible logging
