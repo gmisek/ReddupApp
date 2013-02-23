b23eb7c9b828926bdb6ca016fb849fea54715d2c
gmisek <gmisek@gmail.com>
2013-02-23 11:59:23 -0500

    hook up admin url and index view

--- reddup/reddup/urls.py
+++ reddup/reddup/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
+from scru import views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,5 +14,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'scru.views.index', name='index'),
+   # url(r'/^$',)
 )
