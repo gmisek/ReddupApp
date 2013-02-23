d45841b10df84049dd495079ac39ee25eb028e62
Jim Skerritt <jrskerritt@gmail.com>
2013-02-23 12:11:24 -0500

    Adds sample issue point json

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,21 @@
 /* MAIN JS */
 
-angular.module('steelCityReddUp', ['ui'])
+/*
+	UI Modes:
+		1. default
+		2. identify
+		3. details
+		4. claim
+		5. solve
+*/
 
+angular.module('steelCityReddUp', ['ui'])
 
 /*
 	Maps
 */
-function MapController($scope) {
+function Controller($scope) {
+	$scope.mode = 'default';
 	$scope.markers = [];
 
 	$scope.mapOptions = {
@@ -17,9 +26,11 @@ function MapController($scope) {
 
 	$scope.fetchPoints = function() {
 		$scope.clearMarkers();
+		$('#spinner').spin();
 		$.getJSON(
 			"url",
 			function (data) {
+				$('#spinner').stop();
 				$.each(data.items, function(item){
 
 				});
@@ -39,8 +50,21 @@ function MapController($scope) {
 			marker.setMap(null);
 		});
 	}
-
-	$scope.openInfoWindow = function(marker) {
-
-	}
 }
+
+var samplePoint = {
+	id 				: 1,
+	description 	: "Found some graffiti on the side of an 
+					   office building on Penn Ave",
+	latLng 			: new LatLng(40.462013, 79.926331),
+	beforeImg 		: "before.jpg",
+	afterImg 		: "afterImg",
+	dateOpened		: new Date("Sat Feb 23 2013 11:48:20 GMT-0500 (EST)"),
+	dateClosed		: new Date("Sun Feb 24 2013 12:48:20 GMT-0500 (EST)"), 
+	opener			: { id: 2, username: "jim", deleted: false },
+	closer			: null,
+	cleanerId		: { id: 3, username: "nate", deleted: false },
+	category		: { id: 5, name: "Graffiti", appKey: "GRAFFITI" },
+	locationType	: { id: 5, name: "Street", appKey: "STREET" }
+	reportedTo311	: true,
+};
