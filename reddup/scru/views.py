8d9a195dffef094bde1a28e53d5cc3445f10bdd1
Ben <benspaul@gmail.com>
2013-02-23 14:28:19 -0500

    view for creating an issue

--- reddup/scru/views.py
+++ reddup/scru/views.py
@@ -1,23 +1,34 @@
+from .models import Issue
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, render_to_response, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 import json
-
+from django.contrib.gis.geos import Point
 
 def index(request):
     return render_to_response('index.html', {}, RequestContext(request))
 
 @csrf_exempt
 def create_issue(request):
-    if not request.POST:
-        return
-    desc = request.POST.get('description')
-    before_img = request.POST.get('img')
-    opener = request.POST.get('opener')
-    category = request.POST.get('category')
-    location = request.POST.get('location')
+    if request.method == 'POST':
+        status = 'open'
+        opener_id = request.POST.get('opener_id')
+        description = request.POST.get('description')
+        before_img = request.POST.get('before_img')
+        category_id = request.POST.get('category_id')
+        reported_to_311 = request.POST.get('reported_to_311')
+        location_type_id = request.POST.get('location_type_id')
+        longitude = request.POST.get('longitude')
+        latitude = request.POST.get('latitude')
+        pnt = Point(longitude, latitude)
+        issue = Issue(status=status, opener_id=opener_id, description=description, before_img=before_img,
+                      category_id=category_id, reported_to_311=reported_to_311, location_type_id=location_type_id,
+                      geom=pnt)
+        issue.save()
+        return HttpResponse(json.dumps({'success':True}), content_type='application/json')
+    else:
+        return HttpResponse("you didn't POST any data, bro")
+
 
     # call create here
-
-    return HttpResponse(json.dumps({'success':True}), content_type='application/json')
